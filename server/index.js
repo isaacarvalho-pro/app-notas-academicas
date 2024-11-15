0f719ac44fe1aab4a1eb0b16652b60604bf94cf4
isaacarvalho-pro <isaacarvalho_pro@outlook.com>
2024-11-15 18:55:48 -0300

    primeira conexão com mongoDB

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const { connectToDatabase } = require('./connection');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Servidor rodando!');
-});
-
-app.listen(PORT, () => {
-    console.log(`Servidor iniciado na porta ${PORT}`);
-});
- 
+connectToDatabase() // Estabelece conexão com o banco
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Não foi possível iniciar o servidor:", error);
+  });
