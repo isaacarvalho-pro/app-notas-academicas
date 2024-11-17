79080dd9f76642f5f91bc87638b97a0cae73ecc4
isaacarvalho-pro <isaacarvalho_pro@outlook.com>
2024-11-16 23:49:27 -0300

    criando pagina de cadastro

--- client/main.js
+++ client/main.js
@@ -3,13 +3,16 @@ import noteSvg from './note.svg'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${noteSvg}" class="logo vanilla" alt="JavaScript logo" />
+    <a href="" target="_blank">
+      <img src="${noteSvg}" class="logo note" alt="logo" />
     </a>
     <h1>Bem vindo ao app de anotações academicas!</h1>
     <p class="read-the-docs">
-      Utilize para fazer anotações, classificar a dificuldade das materias e mais...
+      Utilize para fazer anotações, classificar a dificuldade das disciplinas e mais...
     </p>
+    <a href="./src/html/cadastro.html" class="button">
+      Começar!
+    </a>
   </div>
 `
 
